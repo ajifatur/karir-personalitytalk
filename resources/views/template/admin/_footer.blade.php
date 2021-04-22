e3415a8674c8c50148e63330ca12a2e5fdf0a508
isna prasetyo <praseetyaa@gmail.com>
2021-04-22 13:00:31 +0700

    change footer copyright

--- resources/views/template/admin/_footer.blade.php
+++ resources/views/template/admin/_footer.blade.php
@@ -2,7 +2,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; PT. Campus Data Media {{ date('Y') }}. All rights reserved.</span>
+            <span>PersonalityTalk &copy; {{ date('Y') }}. All rights reserved.</span>
           </div>
         </div>
       </footer>
