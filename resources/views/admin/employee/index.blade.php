823c042b1c9ce2443fc50e704297a69998f60880
Fathurrahman Prasetyo Aji <32100684+ajifatur@users.noreply.github.com>
2022-01-27 23:10:59 +0700

    Sync with spandiv.js

--- resources/views/admin/employee/index.blade.php
+++ resources/views/admin/employee/index.blade.php
@@ -67,7 +67,9 @@
 
 <script type="text/javascript">
     // DataTable
-    Spandiv.DataTableServerSide("#datatable", {
+    Spandiv.DataTable("#datatable", {
+		serverSide: true,
+		pageLength: 25,
         url: Spandiv.URL("{{ route('admin.employee.index') }}", {hrd: "{{ Request::query('hrd') }}"}),
         columns: [
             {data: 'checkbox', name: 'checkbox', className: 'text-center'},
@@ -83,10 +85,6 @@
 
     // Button Delete
     Spandiv.ButtonDelete(".btn-delete", ".form-delete");
-
-    // Checkbox
-    Spandiv.CheckboxOne();
-    Spandiv.CheckboxAll();
   
     // Change the HRD
     $(document).on("change", ".card-header select[name=hrd]", function() {
