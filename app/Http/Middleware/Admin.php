b12cf96a81e2a1d280f3ccfb50730d6097ea969d
Aji Fatur <ajifatur32@gmail.com>
2022-02-11 18:41:25 +0700

    Add activity log

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class Admin
 {
@@ -16,6 +17,19 @@ class Admin
      */
     public function handle($request, Closure $next, $guard = null)
     {
+		$primaryKey = (new \App\Models\User())->getKeyName();
+		Log::build([
+            'driver' => 'single',
+            'path' => storage_path('logs/activities.log'),
+        ])->info(
+            json_encode([
+                'user_id' => Auth::guard($guard)->check() ? $request->user()->{$primaryKey} : null,
+                'url' => $request->fullUrl(),
+                'method' => $request->method(),
+                'ip' => $request->ip()
+            ])
+        );
+		
         if (Auth::guard($guard)->check() && ($request->user()->role == role('admin') || $request->user()->role == role('hrd'))) {
             return $next($request);
         }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class RedirectIfAuthenticated
 {
@@ -18,6 +19,19 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+		$primaryKey = (new \App\Models\User())->getKeyName();
+		Log::build([
+            'driver' => 'single',
+            'path' => storage_path('logs/activities.log'),
+        ])->info(
+            json_encode([
+                'user_id' => Auth::guard($guard)->check() ? $request->user()->{$primaryKey} : null,
+                'url' => $request->fullUrl(),
+                'method' => $request->method(),
+                'ip' => $request->ip()
+            ])
+        );
+		
         if (Auth::guard($guard)->check() && ($request->user()->role == role('admin') || $request->user()->role == role('hrd')) && $request->path() == 'login') {
             return redirect()->route('admin.dashboard');
         }
